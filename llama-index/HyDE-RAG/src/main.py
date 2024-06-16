64a6752482b5cf9f615c65e9bc787a9a7c014d8d
Abhishek Ranjan <abhishek.ranjan@sprouts.ai>
2024-06-17 01:26:32 +0530

    checkpoint : indexing of ingested data

--- llama-index/HyDE-RAG/src/main.py
+++ llama-index/HyDE-RAG/src/main.py
@@ -2,15 +2,15 @@ import logging
 import os
 import sys
 import csv
+import chromadb
 from rich.pretty import pprint
 import modin.pandas as pd
-from llama_index.core import SimpleDirectoryReader, VectorStoreIndex
-from llama_index.readers.file import CSVReader, PandasCSVReader  # noqa
+from llama_index.core import Document, VectorStoreIndex
 from llama_index.core.node_parser import SentenceSplitter
-from llama_index.core.ingestion import IngestionPipeline
+from llama_index.core.ingestion import IngestionPipeline  # noqa
 from llama_index.core.extractors import (
     TitleExtractor,
-    QuestionsAnsweredExtractor,
+    QuestionsAnsweredExtractor,  # noqa
     KeywordExtractor,
 )
 from llama_index.embeddings.openai import OpenAIEmbedding  # noqa
@@ -24,29 +24,29 @@ OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 if __name__ == "__main__":
     file_path = "data"
     df = pd.read_csv("data/podcastdata.csv")
-    json_data = df.to_json(orient="records")
-    pprint(json_data[0])
     pprint(df.shape)
+    df = df.head(1)
     # 1. load the data
-    parser = CSVReader()
-    file_extractor = {".csv": parser}
-    documents = SimpleDirectoryReader(
-        file_path, file_extractor=file_extractor
-    ).load_data()
+    documents = [
+        Document(
+            text=row["text"],
+            metadata={
+                "guest": row["guest"],
+                "title": row["title"],
+            },
+        )
+        for index, row in df.iterrows()
+    ]
     pprint(len(documents))
-    # 2. transform the data
-    pipeline = IngestionPipeline(
-        transformations=[
-            SentenceSplitter(chunk_overlap=20, chunk_size=1024),
-            TitleExtractor(nodes=5),
-            QuestionsAnsweredExtractor(questions=3),
-            KeywordExtractor(keywords=5),
-            OpenAIEmbedding(model="text-embedding-3-small"),
-        ]
+    pprint(documents[0])
+    # 2. transform the data and create the index
+    text_splitter = SentenceSplitter(chunk_overlap=20, chunk_size=1024)
+    title_extractor = TitleExtractor(nodes=5)
+    keywords_extractor = KeywordExtractor(nodes=3)
+    index = VectorStoreIndex.from_documents(
+        documents=documents,
+        transformations=[text_splitter, title_extractor, keywords_extractor],
     )
-    nodes = pipeline.run(documents)
-    pprint(f"Number of nodes: {len(nodes)}")
-    # index = VectorStoreIndex(
-    #     nodes=nodes, embed_model=OpenAIEmbedding(model="text-embedding-3-small")
-    # )
-    # store the index in a Vector DB
+
+    # 3. store the index in a vector db of your choice
+    db = chromadb.PersistentClient(path="./chroma_db")
